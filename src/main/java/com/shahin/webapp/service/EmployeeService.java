ab07fbed0f14691205c35f6603ea70692fa414b3
shahin <shahin.aboulhassanibavili2021@campus-eni.fr>
2024-02-14 16:43:04 +0100

    CRUD methods in EmployeeService

--- src/main/java/com/shahin/webapp/service/EmployeeService.java
+++ src/main/java/com/shahin/webapp/service/EmployeeService.java
@@ -5,6 +5,7 @@ import com.shahin.webapp.repository.EmployeeProxy;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 @Data
 @Service
 public class EmployeeService {
@@ -14,5 +15,29 @@ public class EmployeeService {
 
     public Employee getEmployee(final int id) {
         return employeeProxy.getEmployee(id);
+    }
+
+    public Iterable<Employee> getEmployees() {
+        return employeeProxy.getEmployees();
+    }
+
+    public void deleteEmployee(final int id) {
+        employeeProxy.deleteEmployee(id);
+    }
+
+    public Employee saveEmployee(Employee employee) {
+        Employee savedEmployee;
+
+        //Règle de gestion: Le nom de famille doit être mis en majuscule.
+        employee.setLastName(employee.getLastName().toUpperCase());
+
+        if (employee.getId() == null) {
+            //Si l'id est nul, alors c'est un nouvel employé.
+            savedEmployee = employeeProxy.createEmployee(employee);
+        } else {
+            savedEmployee = employeeProxy.updateEmployee(employee);
+        }
+
+        return savedEmployee;
     }
 }
